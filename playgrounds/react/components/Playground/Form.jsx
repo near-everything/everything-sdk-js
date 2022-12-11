ef963e88761a91cdd3a7cbe85b810f9cc22ef380
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-12-11 13:22:12 -0500

    feat: customizable storage in playground

--- playgrounds/react/components/Playground/Form.jsx
+++ playgrounds/react/components/Playground/Form.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { createThing, STORAGE_TYPE } from "@everything-sdk-js/sdk";
-import { useWallet } from "../../../../../mintbase-js/packages/react/lib";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Media from "./Media";
+import { STORAGE_TYPE } from "@everything-sdk-js/sdk";
+import { useState } from "react";
+import { useWallet } from "../../../../../mintbase-js/packages/react/lib";
 import Characteristics from "./Characteristics";
+import Media from "./Media";
 
 function Form() {
   const [files, setFiles] = useState([]);
@@ -11,45 +11,79 @@ function Form() {
   const { selector } = useWallet();
   const { user } = useUser();
   const [thingId, setThingId] = useState("");
-
-  const characteristics = [
-    {
-      attributeId: 1,
-      optionId: 1,
-    },
-  ];
-
+  const [cloud, setCloud] = useState(false);
+  const [blockchain, setBlockchain] = useState(false);
 
   const handleSubmit = async () => {
     const wallet = await selector.wallet();
+    const characteristics = attributes
+      ?.filter(
+        (it) => it.options?.value !== undefined && it.options?.value !== null
+      )
+      .map((attr) => {
+        return {
+          attributeId: parseInt(attr.value),
+          optionId: parseInt(attr.options.value),
+        };
+      });
+    const storage = [];
+    if (cloud) {
+      storage.push(STORAGE_TYPE.CLOUD);
+    }
+    if (blockchain) {
+      storage.push(STORAGE_TYPE.BLOCKCHAIN);
+    }
     const createThingData = {
       user,
       wallet,
-      storage: [
-        STORAGE_TYPE.OFFLINE,
-        STORAGE_TYPE.CLOUD,
-        STORAGE_TYPE.NEAR
-      ],
+      storage,
       characteristics,
-      files
-    }
-    const id = await createThing(createThingData);
-    setThingId(id);
+      files,
+    };
+    console.log(createThingData);
+    // const id = await createThing(createThingData);
+    // setThingId(id);
   };
 
   return (
-      <div className="flex flex-col items-center w-full h-full">
-        <Media files={files} setFiles={setFiles} />
-        <br />
-        <Characteristics attributes={attributes} setAttributes={setAttributes} />
-        <br />
-        <button className="btn w-32" onClick={handleSubmit}>
-          submit
-        </button>
-        <div className="flex w-1/2">
-          <p>response: {thingId}</p>
+    <div className="flex flex-col items-center w-full h-full">
+      <Media files={files} setFiles={setFiles} />
+      <br />
+      <Characteristics attributes={attributes} setAttributes={setAttributes} />
+      <br />
+      <div className="flex flex-col w-1/2">
+        <p>storage</p>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">cloud</span>
+            <input
+              type="checkbox"
+              checked={cloud}
+              className="checkbox"
+              onChange={() => setCloud(!cloud)}
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">blockchain</span>
+            <input
+              type="checkbox"
+              checked={blockchain}
+              className="checkbox"
+              onChange={() => setBlockchain(!blockchain)}
+            />
+          </label>
         </div>
       </div>
+      <br />
+      <button className="btn w-32" onClick={handleSubmit}>
+        submit
+      </button>
+      <div className="flex w-1/2">
+        <p>response: {thingId}</p>
+      </div>
+    </div>
   );
 }
 
